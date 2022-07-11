41419d83d4153a9029d18f9d04bf41467a9d36f5
Ahmed-AlR-amadhani <ahmed.al-ramadhani@wsu.edu>
2022-07-11 11:40:07 -0700

    Monday Matlab coding July 11

--- Matlab/DayTwoCoding.m
+++ Matlab/DayTwoCoding.m
@@ -10,3 +10,22 @@ end
 
 %%
 
+A=[1 2 ; 1 2];
+%%
+%D=randi([1 6],1,6);
+%E=randi([1,6],1,6);
+n=1000;
+D1=0;
+D2=0;
+
+x=0;
+
+while D1~=6 || D2~=6 %the condition of the while loop ...
+    % (it loop will keep on going until the condition is met%...
+    %when D1,D2 aren't equal to 6 then it will keep going 
+    D1=randi(6); %makes this equation keep on going%
+    D2=randi(6); 
+    x=x+1; %counts how many times it took !%
+end
+disp(x)
+disp([D1,D2])
