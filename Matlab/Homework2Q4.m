f75e68cff9892ddaf6e1e3bd610c0fdd392c0769
Ahmed-AlR-amadhani <ahmed.al-ramadhani@wsu.edu>
2022-07-13 11:49:20 -0700

    homework2

--- Matlab/Homework2Q4.m
+++ Matlab/Homework2Q4.m
@@ -6,16 +6,16 @@
 %%Question4
 
 D1=0;
-D2=0;
+D=0;
 
 x=0;
 
-while D<=20 %the condition of the while loop ...
+while D<20 %the condition of the while loop ...
     % (it loop will keep on going until the condition is met%...
     %when D1,D2 aren't equal to 6 then it will keep going 
     D1=rand(1); %makes this equation keep on going%
-    D2=D2+D1;
+    D=D+D1;
     x=x+1; %counts how many times it took !%
 end
 disp(x)
-disp(D2)
+disp(D)
