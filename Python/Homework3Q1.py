f545245abdb80ade3fd4ada8027f218d2ad617e3
Ahmed-AlR-amadhani <ahmed.al-ramadhani@wsu.edu>
2022-07-20 11:12:39 -0700

    hw

---
+++ Python/Homework3Q1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 20 08:23:17 2022
+
+@author: ahmedalramadhani
+"""
+ 
+import numpy as np
+import sympy as sym
+import scipy as sci
+a=np.array([[1, 2],[3,4]])
+b=np.array([5],[8])
+x=np.linlang.solve(a,b)
+
---
+++ Python/Homework3Q3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 20 08:24:20 2022
+
+@author: ahmedalramadhani
+"""
+
